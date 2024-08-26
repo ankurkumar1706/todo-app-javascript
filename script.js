0d1476e54ad35f7c281621fc52a6c954611138e1
Ankur Choudhary <ankur@Ankurs-MacBook-Pro.local>
2024-08-26 10:22:46 +0530

    create task  add function

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 "use strict";
+
 // Initial empty array to store todos
 let allTasks = [];
 
 // Function to add a new todo
 const addTask = () => {
-  const taskInput = getElementById("taskInput");
+  const taskInput = document.getElementById("taskInput");
   const taskText = taskInput.value.trim();
 
   if (taskText !== "") {
     allTasks.push(taskText);
     taskInput.value = ""; // Clear the input field
-
-    renderTodos();
   }
   console.log(allTasks);
 };
+
+// task create on button click
+document.getElementById("addTaskBtn").addEventListener("click", function (e) {
+  e.preventDefault();
+  addTask();
+});
